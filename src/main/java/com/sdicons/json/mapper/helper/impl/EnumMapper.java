50914917d33875703c3831f641bd9fc691b4675f
Bruno Ranschaert <bruno.ranschaert@sdi-consulting.com>
2012-11-29 23:23:15 +0100

    Added EnumMapperTest.

--- src/main/java/com/sdicons/json/mapper/helper/impl/EnumMapper.java
+++ src/main/java/com/sdicons/json/mapper/helper/impl/EnumMapper.java
@@ -11,36 +11,31 @@ import com.sdicons.json.model.JSONString;
 import com.sdicons.json.model.JSONValue;
 
 public class EnumMapper
-extends AbstractMapper
-{
+extends AbstractMapper {
+
     private static final String EM001 = "JSONMapper/EnumMapper/001: JSON->Java. Cannot map JSON class '%s' to Java Enum.";
     private static final String EM002 = "JSONMapper/EnumMapper/002: JSON->Java. Failed to handle a non-enum class '%s'.";
     private static final String EN003 = "JSONMapper/EnumMapper/003: JSON->Java. The enum class '%s' was found but no matching enum value could be found for '%s'.";
 
-    public Class<?> getHelpedClass()
-    {
+    public Class<?> getHelpedClass() {
         return Enum.class;
     }
 
-    public Object toJava(JSONMapper mapper, JSONValue aValue, Class<?> aRequestedClass)
-    throws MapperException
-    {
+    public Object toJava(JSONMapper mapper, JSONValue aValue, Class<?> aRequestedClass) throws MapperException {
 
-        if (!aValue.isString()) throw new MapperException(String.format(EM001, aValue.getClass().getName()));
+        if (!aValue.isString())
+            throw new MapperException(String.format(EM001, aValue.getClass().getName()));
 
-        if(aRequestedClass.isEnum())
-        {
+        if (aRequestedClass.isEnum()) {
             Object[] lEnumVals = aRequestedClass.getEnumConstants();
-            for(Object lEnumVal: lEnumVals)
-            {
-                if(lEnumVal.toString().equals(((JSONString)aValue).getValue())) return lEnumVal;
+            for (Object lEnumVal : lEnumVals) {
+                if (lEnumVal.toString().equals(((JSONString) aValue).getValue())) return lEnumVal;
             }
         }
-        else
-        {
+        else {
             throw new MapperException(String.format(EM002, aRequestedClass.getName()));
         }
 
-        throw new MapperException(String.format(EN003, aRequestedClass.getName(), ((JSONString)aValue).getValue()));
+        throw new MapperException(String.format(EN003, aRequestedClass.getName(), ((JSONString) aValue).getValue()));
     }
 }
---
+++ src/test/java/com/sdicons/json/mapper/helper/impl/EnumMapperTest.java
@@ -0,0 +1,60 @@
+package com.sdicons.json.mapper.helper.impl;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sdicons.json.mapper.JSONMapper;
+import com.sdicons.json.mapper.MapperException;
+import com.sdicons.json.mapper.helper.MapperHelper;
+import com.sdicons.json.model.JSONBoolean;
+import com.sdicons.json.model.JSONString;
+import com.sdicons.json.model.JSONValue;
+
+public class EnumMapperTest {
+    private EnumMapper helper;
+    private JSONMapper mapper;
+
+    @Before
+    public void init() {
+        helper = new EnumMapper();
+        mapper = new JSONMapper(new MapperHelper[]{});
+    }
+
+    public static enum  Qualification { AWFUL, BAD, NORMAL, GOOD, SUPER };
+
+    @Test
+    public void enums() throws MapperException {
+        Qualification q = Qualification.NORMAL;
+        JSONValue json = helper.toJSON(mapper, q);
+        Assert.assertNotNull(json);
+        Assert.assertThat(json, is(instanceOf(JSONString.class)));
+        //
+        Object back = helper.toJava(mapper, json, Qualification.class);
+        Assert.assertNotNull(back);
+        Assert.assertEquals(q, back);
+    }
+
+
+    @Test(expected=MapperException.class)
+    public void badInput() throws MapperException {
+        // The string 'AI' cannot be converted to an enum value of Qualification.
+        helper.toJava(mapper, new JSONString("AI"), Qualification.class);
+    }
+
+    @Test(expected=MapperException.class)
+    public void badInput2() throws MapperException {
+        // The requested class is not an enum.
+        helper.toJava(mapper, new JSONString("SUPER"), Integer.class);
+    }
+
+    @Test(expected=MapperException.class)
+    public void badInput3() throws MapperException {
+        // Bad JSON value, it does not represent an enum.
+        helper.toJava(mapper, JSONBoolean.TRUE, Qualification.class);
+    }
+
+}
