c601de68ca64c64b5bdc1dd2863e11ae39d1d97e
Bruno Ranschaert <bruno.ranschaert@sdi-consulting.com>
2012-12-15 16:46:48 +0100

    Removed a number of TODO's from the code.

--- src/main/java/com/sdicons/json/validator/ValidationException.java
+++ src/main/java/com/sdicons/json/validator/ValidationException.java
@@ -17,21 +17,17 @@ extends Exception
 
     public ValidationException() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     public ValidationException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
     }
 
     public ValidationException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
     }
 
     public ValidationException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
     }
 }
